f30f039bee1742f573efd41d0a8b1d875fe40c82
Mike Hamrah <m@hamrah.com>
2020-02-23 09:57:06 -0500

    try 5.0 module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,6 @@
 module "vpc-project" {
   source          = "terraform-google-modules/project-factory/google"
-  version         = "~> 6.0"
+  version         = "~> 5.0"
   org_id          = var.org_id
   billing_account = var.billing_account
   folder_id       = var.folder_id
