a591822fc282189c6037a2aa6d6bde2d1831b14b
Mike Hamrah <m@hamrah.com>
2020-02-22 22:36:55 -0500

    use org id

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ module "root-folders" {
 module "project-audit" {
   source          = "terraform-google-modules/project-factory/google"
   version         = "~> 7.0"
-  org_id          = "${data.google_organization.org.id}"
+  org_id          = "${data.google_organization.org.org_id}"
   billing_account = "${data.google_billing_account.acct.id}"
   name            = "fuzz-audit"
   lien            = true
