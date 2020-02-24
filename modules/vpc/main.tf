7c001c64d7c2b916ae6c37f2f78fcf27c89c1043
Mike Hamrah <m@hamrah.com>
2020-02-23 21:06:54 -0500

    add vpc_host_project

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,3 +5,7 @@ resource "google_project" "vpc_project" {
   billing_account     = var.billing_account
   auto_create_network = false
 }
+
+resource "google_compute_shared_vpc_host_project" "host" {
+  project = google_project.vpc_project.project_id
+}
