a315e0df659fdfcc4b27b85b922b4a5fcf08cbe0
Mike Hamrah <m@hamrah.com>
2020-02-23 20:26:19 -0500

    remove vpc project

--- modules/vpc/main.tf
+++
@@ -1,9 +0,0 @@
-module "vpc-project" {
-  source          = "terraform-google-modules/project-factory/google"
-  version         = "~> 5.0"
-  org_id          = var.org_id
-  billing_account = var.billing_account
-  folder_id       = var.folder_id
-  name            = "${var.name_prefix}-vpc"
-  lien            = true
-}
